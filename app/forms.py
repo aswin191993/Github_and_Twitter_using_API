54dd8f11bfb13f84576e424c3ce3d40d3899a992
aswin191993 <aswin191993@gmail.com>
2015-04-03 23:32:04 +0530

    Flask

---
+++ app/forms.py
@@ -0,0 +1,8 @@
+from flask.ext.wtf import Form
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
+class SigninForm(Form):
+    idu = StringField('idu')
+    name = StringField('name')
+    
---
+++ app/views.py
@@ -0,0 +1,60 @@
+import requests
+from flask import render_template, flash, redirect,url_for,session,abort
+from app import app
+from .forms import SigninForm
+import json
+import oauth2 as oauth,json
+import twitter
+
+def reques(url):
+	consumer = oauth.Consumer(key='GcPPtjWQoHZiLWTpMQa3z44Es',secret='JJo4VbMBKJ6J9FdSl6x10tpGWGh2mEBjc1NzHXxDWCNo3mhILz')
+	token = oauth.Token(key='597987159-cX3oqo8rGFVC0i1WZZuN8hI0qKXjcAQBZlE66un7',secret='7idwwQzFB1TqRmKBcTSFwW79BGKMkykb6c2oT1s2GLWoN')
+	client = oauth.Client(consumer, token)
+	resp,content = client.request( url)
+	return content
+
+@app.route('/',  methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+	form = SigninForm()
+	resp=[]
+	t1="Result:"
+	if form.validate_on_submit():
+		#username=""
+		#usernameg=""		
+		username=form.idu.data
+		usernameg=form.name.data
+		if username !="":
+			t1="Twitter Tweet"
+		else:
+			t1="GitHub Repositories"
+		urlg='https://github.com/'+usernameg+'?tab=repositories' 	
+		v = str('<a class="repo-list-stat-item tooltipped tooltipped-s" href="/'+usernameg+'/')
+		rg =requests.get(urlg)
+		stored=[]
+		count=""
+		decode=rg.text
+		for i in decode:
+			count+=i
+			if i=='\n':
+				stored.append(count)
+				count=""
+		for f in stored:
+			if v in f:
+				h=f.find("/stargazers")
+				if f.find("/stargazers") == h and '/network' not in f: 			
+					resp.append(f[(4+len(v)):h])
+		u=reques('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name='+username+'&count=10')
+		count=""
+		stored=[]
+		r=str(json.loads(u))
+		for i in r:
+			count+=i
+			if i == ",":
+				stored.append(count)
+				count=""
+		for k in stored:
+			if "text" in k and "text_" not in k:
+				resp.append(k[12:-23])
+
+	return render_template('index.html',title='Home',t1=t1,form=form,resp=resp)
